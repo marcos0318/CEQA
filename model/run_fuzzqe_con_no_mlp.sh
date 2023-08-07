a7d16c5748bf7946239cd411edc0a88f821c0731
Jiaxin_Bai <jbai0318@gmail.com>
2023-08-07 00:44:35 -0700

    Add: add one script

---
+++ model/run_fuzzqe_con_no_mlp.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=0 python ./train.py \
+    -m fuzzqe_con_no_mlp \
+    --train_query_dir ../query_data_filtered/query_data_train_filtered.json \
+    --valid_query_dir ../query_data_filtered/query_data_valid_filtered.json \
+    --test_query_dir ../query_data_filtered/query_data_test_filtered.json \
+    --checkpoint_path ../aser_reasoning_logs \
+    -lr 0.0001 \
+    -b 1024 
