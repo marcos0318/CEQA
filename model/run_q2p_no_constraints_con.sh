555c1d75262fb6c216e0ddb2a8e35ac629a371ad
Jiaxin_Bai <jbai0318@gmail.com>
2023-08-16 11:06:49 -0700

    Fix: fix hyper parameters

--- model/run_q2p_no_constraints_con.sh
+++ model/run_q2p_no_constraints_con.sh
@@ -3,6 +3,4 @@ CUDA_VISIBLE_DEVICES=6 python ./train.py \
     --train_query_dir ../query_data_filtered_reversed/query_data_train_filtered.json \
     --valid_query_dir ../query_data_filtered_reversed/query_data_valid_filtered.json \
     --test_query_dir ../query_data_filtered_reversed/query_data_test_filtered.json \
-    --checkpoint_path ../aser_reasoning_logs_no_constraints \
-    -b 512 \
-    -lr 0.0001 \
+    --checkpoint_path ../aser_reasoning_logs_no_constraints 
