ebc97f6904503569c5a45ac09df739060de99f3e
Jiaxin_Bai <jbai0318@gmail.com>
2023-08-16 00:19:22 -0700

    Fix: change paths

--- model/run_gqe_no_constraints.sh
+++ model/run_gqe_no_constraints.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=3 python ./train.py \
     -m gqe \
-    --train_query_dir /home/jbai/logical_aser/query_data_filtered_reversed/query_data_train_filtered.json \
-    --valid_query_dir /home/jbai/logical_aser/query_data_filtered_reversed/query_data_valid_filtered.json \
-    --test_query_dir /home/jbai/logical_aser/query_data_filtered_reversed/query_data_test_filtered.json \
-    --checkpoint_path /home/data/jbai/aser_reasoning_logs_no_constraints \
+   --train_query_dir ../query_data_filtered_reversed/query_data_train_filtered.json \
+    --valid_query_dir ../query_data_filtered_reversed/query_data_valid_filtered.json \
+    --test_query_dir ../query_data_filtered_reversed/query_data_test_filtered.json \
+    --checkpoint_path ../aser_reasoning_logs_no_constraints \
     -b 256 
--- model/run_q2p_no_constraints.sh
+++ model/run_q2p_no_constraints.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=7 python ./train.py \
     -m q2p \
-   --train_query_dir /home/jbai/logical_aser/query_data_filtered_reversed/query_data_train_filtered.json \
-    --valid_query_dir /home/jbai/logical_aser/query_data_filtered_reversed/query_data_valid_filtered.json \
-    --test_query_dir /home/jbai/logical_aser/query_data_filtered_reversed/query_data_test_filtered.json \
-    --checkpoint_path /home/data/jbai/aser_reasoning_logs_no_constraints \
+   --train_query_dir ../query_data_filtered_reversed/query_data_train_filtered.json \
+    --valid_query_dir ../query_data_filtered_reversed/query_data_valid_filtered.json \
+    --test_query_dir ../query_data_filtered_reversed/query_data_test_filtered.json \
+    --checkpoint_path ../aser_reasoning_logs_no_constraints \
     -b 256 \
     -lr 0.001 
